455b03968458296aa39d53634834413acdee2347
Erik Schoster <erik@hecanjog.com>
2020-08-21 12:44:53 -0500

    Fix twtxt posting script

--- scripts/twtxt.sh
+++ scripts/twtxt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#`date -Im`\t$1 >> static/twtxt.txt
+set -e
 POSTTIME=`date -Im`
 TMPFILE=/tmp/$POSTTIME.twtxt.txt
-echo "$POSTTIME\t" >> $TMPFILE
-vim $TMPFILE && cat $TMPFILE >> static/twtxt.txt
+vim $TMPFILE
+if test -f "$TMPFILE"; then
+    echo -e "$POSTTIME\t`cat $TMPFILE`" >> static/twtxt.txt
+fi
