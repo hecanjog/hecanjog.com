e59188582ee64a7189bae67fc782c728fab5772d
Erik Schoster <erik@hecanjog.com>
2020-11-14 18:19:54 -0600

    Rework blog and listening a bit

--- engine/build.py
+++ engine/build.py
@@ -46,7 +46,6 @@ class Post:
         slug = unicodedata.normalize('NFKD', self.title).encode('ascii', 'ignore').decode('ascii')
         slug = re.sub('[-\s]+', '-', re.sub('[^\w\s-]', '', slug).strip().lower())
         self.slug = '%04d%02d%02d-%s' % (self.year, self.month, self.day, slug)
-        self.permalink = '/posts/%s.html' % (self.slug)
 
 
 def load_template():
@@ -72,76 +71,51 @@ def build_pages():
             converted = pypandoc.convert_file(str(source), 'html5', format='md')
             html.write(header + converted + footer)
 
-def build_blog_posts():
+def build_posts(name):
     header, footer = load_template()
 
-    with open('templates/blog.html', 'r', encoding='utf-8') as f:
-        bloghome = f.read()
+    with open('templates/%s.html' % name, 'r', encoding='utf-8') as f:
+        postshome = f.read()
 
     # Blog
-    blog = []
-    for p in Path('./posts/blog/').glob('*.md'):
+    posts = []
+    for p in Path('./posts/%s/' % name).glob('*.md'):
         post = Post(p)
         if post.status.lower() != 'draft':
-            blog += [ post ]
+            posts += [ post ]
 
-    blog.sort(key=lambda p: p.date, reverse=True)    
-    with open('static/blog.html', 'w', encoding='utf-8') as html:
+    posts.sort(key=lambda p: p.date, reverse=True)    
+    with open('static/%s.html' % name, 'w', encoding='utf-8') as html:
         html.write(header)
 
-        bloglist = '<ul class="bloglist">'
-        for p in blog:
-            bloglist += '<li><h2><a href="%s">%s</a></h2><p class="byline">Posted on %s</p></li>' % (p.permalink, p.title, p.datestring)
-            with open('static/posts/%s.html' % p.slug, 'w', encoding='utf-8') as blogpagehtml:
-                print('Blog: %s - %s' % (p.title, p.datestring))
-                blogpagehtml.write(header)
-                blogpagehtml.write('<div class="blog post">')
-                blogpagehtml.write('<h2>%s</h2>' % p.title)
-                blogpagehtml.write('<p class="byline">Posted on %s</p>' % p.datestring)
-                blogpagehtml.write('<hr/>')
-                p.html = p.html.replace('/images/', 'img/')
-                blogpagehtml.write(p.html)
-                blogpagehtml.write('</div>')
-                blogpagehtml.write(footer)
-        bloglist += '</ul>'
+        postslist = '<ul class="%slist">' % name
+        for p in posts:
+            postslist += '<li><h2><a href="/%s/%s.html">%s</a></h2><p class="byline">Posted on %s</p></li>' % (name, p.slug, p.title, p.datestring)
+            with open('static/%s/%s.html' % (name, p.slug), 'w', encoding='utf-8') as postspagehtml:
+                print('%s: %s - %s' % (name, p.title, p.datestring))
+                postspagehtml.write(header)
+                postspagehtml.write('<div class="%s post">' % name)
+                postspagehtml.write('<h2>%s</h2>' % p.title)
+                postspagehtml.write('<p class="byline">Posted on %s</p>' % p.datestring)
+                postspagehtml.write('<hr/>')
+                p.html = p.html.replace('/images/', '/img/')
+                p.html = p.html.replace('{static}', '')
+                postspagehtml.write(p.html)
+                postspagehtml.write('</div>')
+                postspagehtml.write(footer)
+        postslist += '</ul>'
 
-        bloghome = bloghome.replace('$BLOGLIST', bloglist)
-        html.write(bloghome)
-
-        html.write(footer)
-
-def build_listening_posts():
-    header, footer = load_template()
-
-    # Listening Journal
-    listening = []
-    for p in Path('./posts/listening/').glob('*.md'):
-        post = Post(p)
-        if post.status.lower() != 'draft':
-            listening += [ post ]
-
-    listening.sort(key=lambda p: p.date, reverse=True)    
-    with open('static/listening.html', 'w', encoding='utf-8') as html:
-        html.write(header)
-        html.write('<h2>Listening Journal</h2>')
-        for p in listening:
-            print('Listening: %s - %s' % (p.title, p.datestring))
-            html.write('<hr/>')
-            html.write('<div class="listening post">')
-            html.write('<h3>%s</h3>' % p.title)
-            p.html = p.html.replace('/images/', 'img/')
-            html.write(p.html)
-            html.write('</div>')
-
+        postshome = postshome.replace('$%sLIST' % name.upper(), postslist)
+        html.write(postshome)
         html.write(footer)
 
 
 if __name__ == '__main__':
     if sys.argv[1] == 'blog':
-        build_blog_posts()
+        build_posts('blog')
 
     if sys.argv[1] == 'listening':
-        build_listening_posts()
+        build_posts('listening')
 
     if sys.argv[1] == 'pages':
         build_pages()
